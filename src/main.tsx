277c645df6a239b9196cc38726d087c03ce066c7
Ozan Yilmaz <ozanyilmaz82@gmail.com>
2025-07-16 12:26:10 -0400

    Fully removed Tina CMS from project

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { TinaWrapper } from './components/TinaProvider.tsx'
+// import { TinaWrapper } from './components/TinaProvider.tsx'
 
 console.log("Starting app...");
 createRoot(document.getElementById("root")!).render(
   <div className="dark">
-    <TinaWrapper>
-      <App />
-    </TinaWrapper>
+    <App />
   </div>
 );
